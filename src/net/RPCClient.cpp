f6c7df94bcf47c45c0966b68cf57a5682f55e99c
ruihong123 <wang4996@purdue.edu>
2021-02-14 13:09:15 -0500

    commit 0214

--- src/net/RPCClient.cpp
+++ src/net/RPCClient.cpp
@@ -85,9 +85,9 @@ bool RPCClient::RdmaCall(uint16_t DesNodeID, char *bufferSend, uint64_t lengthSe
 //    asm volatile ("lfence\n" : : );
 //    usleep(10);
 //    for (i=0; i<100000000; i++);
-//    _mm_clflush(recv);
+//
     while (1) {
-
+        _mm_clflush(recv);
         if(recv->message == MESSAGE_RESPONSE)
             break;
 //        i++; //this i++ is neccessary otherwise the program will be stuck here.
