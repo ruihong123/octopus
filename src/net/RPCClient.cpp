3ee050a6896065ef4de5ac07988b4a064e93b84e
ruihong123 <wang4996@purdue.edu>
2021-02-13 22:50:35 -0500

    remove the unitialized warning

--- src/net/RPCClient.cpp
+++ src/net/RPCClient.cpp
@@ -79,10 +79,11 @@ bool RPCClient::RdmaCall(uint16_t DesNodeID, char *bufferSend, uint64_t lengthSe
 //		while (recv->message != MESSAGE_RESPONSE){}
 //	} else {
 //		// gettimeofday(&startt,NULL);
-    int i = 0;
+//    int i = 0;
     Debug::debugItem("Come to the while loop");
     while (recv->message != MESSAGE_RESPONSE) {
-        i++; //this i++ is neccessary otherwise the program will be stuck here.
+        usleep(10);
+//        i++; //this i++ is neccessary otherwise the program will be stuck here.
 //        printf("recv message: %d\n", recv->message);
 //        printf("lengthreceive: %d", lengthReceive);
 			/* gettimeofday(&endd,NULL);
@@ -97,7 +98,7 @@ bool RPCClient::RdmaCall(uint16_t DesNodeID, char *bufferSend, uint64_t lengthSe
 				diff = 0;
 			}*/
     }
-    usleep(10);
+
 //	}
 	memcpy((void*)bufferReceive, (void *)receiveBuffer, lengthReceive);
 	return true;
